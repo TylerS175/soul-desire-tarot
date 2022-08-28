498607af1b08de1072502dba0223aa899de3a2c8
Tyler Shifflett <tylershifflett@Tylers-MacBook-Pro.local>
2022-08-28 16:27:46 -0400

    Add Two Price Options

--- src/pages/Reading.js
+++ src/pages/Reading.js
@@ -8,14 +8,18 @@ function Reading(){
             <h1 className="text-center">Book a Private Reading</h1>
             <br></br>
             <h2 className="text-center">Please see the options below. Once you chosen the reading for please select the purchase button and from it will redirect you to linktree to complete your purchase. Three payment options: Cashapp, Venmo, or Paypal.</h2>
+            <button className="">Purchase</button>
             <br></br>
-                <h1>Love Reading</h1>
+                <h1>Love Reading - $50</h1>
             <div className="row">
-                <img className="photo" src={process.env.PUBLIC_URL + '/images/2ofcups .jpeg'} alt="7" />
-                <p></p>
-
+                <img className="photo" src={process.env.PUBLIC_URL + '/images/2ofcups .jpeg'} alt="2" />
+                <p>Currently in a relationship or do you have someone in mind?</p>
             </div>
-            <button>purchase</button>
+                <h1>Career Reading/Life Purpose - $50</h1>
+            <div className="row">
+                <img className="photo" src={process.env.PUBLIC_URL + '/The Emperor Tarot .jpeg'} alt="3" />
+                <p></p>
+            </div>
            
         </div>
     );
